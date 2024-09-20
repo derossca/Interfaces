8f9f6351aa1439ccb18b508afbd9793cd485d693
derossca <derossca@mail.uc.edu>
2024-09-20 13:47:27 -0400

    made a data test for ShortLister, made ShortLister and tested it

--- src/ShortLister.java
+++ src/ShortLister.java
@@ -1,2 +1,85 @@
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
 public class ShortLister {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+        ArrayList<String> things = new ArrayList<>();
+
+        try {
+
+            // use the toolkit to get the current working directory of the IDE
+            // Not sure if the toolkit is thread safe...
+            File workingDirectory = new File(System.getProperty("user.dir"));
+
+            // Typiacally, we want the user to pick the file so we use a file chooser
+            // kind of ugly code to make the chooser work with NIO.
+            // Because the chooser is part of Swing it should be thread safe.
+            chooser.setCurrentDirectory(workingDirectory);
+            // Using the chooser adds some complexity to the code.
+            // we have to code the complete program within the conditional return of
+            // the filechooser because the user can close it without picking a file
+
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+                // Typical java pattern of inherited classes
+                // we wrap a BufferedWriter around a lower level BufferedOutputStream
+                InputStream in =
+                        new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(in));
+
+                // Finally we can read the file LOL!
+                //int line = 0; this is if we wanted to tally the lines
+                while (reader.ready()) {
+                    rec = reader.readLine();
+                    //line++; this is if we wanted to tally the lines
+                    // echo to screen
+                    things.add(rec);
+                }
+                reader.close(); // must close the file to seal it and flush buffer
+
+                Filter shortWordFilter = new ShortWordFilter();
+
+                for (String stuff : things) {
+                    String[] words = stuff.split(" ");
+                    for (String word : words) {
+                        if(shortWordFilter.accept(word)){
+                            System.out.println(word);
+                        }
+                    }
+                }
+
+                System.out.println("\n\nData file read!");
+
+
+            } else  // User closed the chooser without selecting a file
+            {
+                System.out.println("No file selected!!! ... exiting.\nRun the program again and select a file.");
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!!!");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
--- src/ShortWordFilter.java
+++ src/ShortWordFilter.java
@@ -1,7 +1,9 @@
 public class ShortWordFilter implements Filter {
     @Override
     public boolean accept(Object x){
-        String shortWord = (String) x;
-        return shortWord.length() < 5;
+        if (x instanceof String){
+            return ((String)x).length() < 5;
+        }
+        return false;
     }
 }
